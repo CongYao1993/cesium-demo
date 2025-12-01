2ee8b1ce1af3de42b3b53fdbdaaa17cc1f9d45c6
yaocong <yaocong@ia.ac.cn>
2025-12-01 16:29:35 +0800

    feat: Viewer配置；

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,17 @@ if (typeof window !== "undefined") {
 }
 
 import { useEffect, useRef } from "react";
-import { Ion, Terrain, Viewer } from "cesium";
+import { Camera, Ion, Rectangle, Terrain, Viewer } from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 
 // Your access token can be found at: https://ion.cesium.com/tokens.
 // This is the default access token from your ion account
 Ion.defaultAccessToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlZDdiMzcwYy1hNjcwLTQ0YTUtYWZmYS0yY2E5NTBmZDc2MTEiLCJpZCI6MzM3NTAwLCJpYXQiOjE3NTY4MDM4NTd9.4Lq_uU3n4SSDdy_FmPka-9Zg7eOIb-wc1L7yQw_dmv4";
+
+// 设置默认视角为中国
+// 参数分别为：西边经度、南边纬度、东边经度、北边纬度
+Camera.DEFAULT_VIEW_RECTANGLE = Rectangle.fromDegrees(89.5, 20.4, 110.4, 61.2);
 
 export default function Home() {
   const cesiumContainerRef = useRef<HTMLDivElement>(null);
@@ -23,7 +27,16 @@ export default function Home() {
     if (!cesiumContainerRef.current) return;
 
     // Initialize the Cesium Viewer
-    const viewer = new Viewer(cesiumContainerRef.current);
+    const viewer = new Viewer(cesiumContainerRef.current, {
+      geocoder: false, // 不会创建地理位置搜索工具
+      homeButton: false, // 不会创建 HomeButton
+      sceneModePicker: false, // 不会创建 2D、3D、CV 模式切换按钮
+      baseLayerPicker: false, // 不会创建地图选择按钮
+      navigationHelpButton: false, // 不会创建帮助按钮
+      animation: false, // 不会创建动画部件
+      timeline: false, // 不会创建时间轴部件
+      fullscreenButton: false, // 不会创建全屏按钮
+    });
 
     viewerRef.current = viewer;
 
